11b3fd4f3aabf273697950bfa3fcca11b7a45ab7
Brian Caulkins <briancaulkins@gmail.com>
2022-02-12 11:04:31 -0600

    add letter count verification; expanded word list

--- turdle.py
+++ turdle.py
@@ -1,5 +1,13 @@
+import os, random
 from time import sleep
-import random
+
+def screen_clear():
+   # for mac and linux(here, os.name is 'posix')
+   if os.name == 'posix':
+      _ = os.system('clear')
+   else:
+      # for windows platfrom
+      _ = os.system('cls')
 
 #sets path to list of words for the game.
 filepath = 'turdles.txt'
@@ -37,32 +45,38 @@ guesses = 1
 while guesses < 7:
     print ('_ _ _ _ _')
     guess_1 = input ('Guess ' +str(guesses)+':')
-    guess_1a = list(guess_1)
-    if guess_1 == turdle:
-        if guesses == 1:
-            print('Congrats! You guessed the TURDLE in ' + str(guesses) + ' guess ! Have a nice day!')
-            sleep(10)
-            quit()
-        else:
-            print('Congrats! You guessed the TURDLE in ' +str(guesses) + ' guesses ! Have a nice day!')
-            sleep(10)
-            quit()
-    if guess_1a[0]==l1:
-        print(guess_1a[0])
-    else: print('_')
-    if guess_1a[1]==l2:
-        print(guess_1a[1])
-    else: print('_')
-    if guess_1a[2]==l3:
-        print(guess_1a[2])
-    else: print('_')
-    if guess_1a[3]==l4:
-        print(guess_1a[3])
-    else: print('_')
-    if guess_1a[4]==l5:
-        print(guess_1a[4])
-    else: print('_')
-    guesses=guesses+1
+    if len(guess_1) != 5:
+        screen_clear()
+        print('Word guess is not the correct length. Try again.')        
+    else:
+        guess_1a = list(guess_1)
+        if guess_1 == turdle:
+            if guesses == 1:
+                print('Congrats! You guessed the TURDLE in ' + str(guesses) + ' guess ! Have a nice day!')
+                sleep(10)
+                quit()
+            else:
+                print('Congrats! You guessed the TURDLE in ' +str(guesses) + ' guesses ! Have a nice day!')
+                sleep(10)
+                quit()
+        screen_clear()
+        if guess_1a[0]==l1:
+            print(guess_1a[0])
+        else: print('_')
+        if guess_1a[1]==l2:
+            print(guess_1a[1])
+        else: print('_')
+        if guess_1a[2]==l3:
+            print(guess_1a[2])
+        else: print('_')
+        if guess_1a[3]==l4:
+            print(guess_1a[3])
+        else: print('_')
+        if guess_1a[4]==l5:
+            print(guess_1a[4])
+        else: print('_')
+        guesses=guesses+1
+        
 print(failed)
 #print('Game Over. Please Try Again.')
 sleep(10)
